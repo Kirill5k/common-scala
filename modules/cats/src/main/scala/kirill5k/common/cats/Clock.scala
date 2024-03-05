c2adbda09d94776d3912fc1f35ef42da59c2fcc5
kirill5k <immotional@aol.com>
2024-03-05 11:14:55 +0000

    MockClock

--- modules/cats/src/main/scala/kirill5k/common/cats/Clock.scala
+++ modules/cats/src/main/scala/kirill5k/common/cats/Clock.scala
@@ -1,5 +1,6 @@
 package kirill5k.common.cats
 
+import cats.Monad
 import cats.effect.Temporal
 import cats.syntax.functor.*
 
@@ -11,15 +12,35 @@ trait Clock[F[_]]:
   def durationBetweenNowAnd(otherTs: Instant): F[FiniteDuration]
   def sleep(duration: FiniteDuration): F[Unit]
 
+  protected def durationBetween(ts: Instant, otherTs: Instant): FiniteDuration =
+    math.abs(otherTs.toEpochMilli - ts.toEpochMilli).millis
+
 final private class LiveClock[F[_]](using F: Temporal[F]) extends Clock[F] {
-  override def now: F[Instant]                          = F.realTimeInstant
-  override def sleep(duration: FiniteDuration): F[Unit] = F.sleep(duration)
-  override def durationBetweenNowAnd(otherTs: Instant): F[FiniteDuration] =
-    now.map(ts => math.abs(otherTs.toEpochMilli - ts.toEpochMilli).millis)
+  override def now: F[Instant]                                            = F.realTimeInstant
+  override def sleep(duration: FiniteDuration): F[Unit]                   = F.sleep(duration)
+  override def durationBetweenNowAnd(otherTs: Instant): F[FiniteDuration] = now.map(ts => durationBetween(ts, otherTs))
 }
 
-object Clock:
+final private class MockClock[F[_]: Monad](
+    private var timestamp: Instant
+)(using
+    M: Monad[F]
+) extends Clock[F] {
+  override def now: F[Instant]                                            = M.pure(timestamp)
+  override def durationBetweenNowAnd(otherTs: Instant): F[FiniteDuration] = M.pure(durationBetween(timestamp, otherTs))
+  override def sleep(duration: FiniteDuration): F[Unit]                   = M.pure(set(timestamp.plusNanos(duration.toNanos)))
+
+  def set(newTimestamp: Instant): Unit = {
+    timestamp = newTimestamp
+    ()
+  }
+}
+
+object Clock {
   given [F[_]: Temporal]: Clock[F] = Clock.make[F]
 
   def apply[F[_]](using C: Clock[F]): C.type = C
   def make[F[_]: Temporal]: Clock[F]         = LiveClock[F]()
+
+  def mock[F[_]: Monad](ts: Instant): MockClock[F] = MockClock[F](ts)
+}
