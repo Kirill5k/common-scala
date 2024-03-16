659ce2b955ae365ed4e85bdb3ee65eb72a077ce9
kirill5k <immotional@aol.com>
2024-03-16 11:07:16 +0000

    Add types

--- modules/cats-test/src/main/scala/kirill5k/common/cats/test/IOMockitoMatchers.scala
+++ modules/cats-test/src/main/scala/kirill5k/common/cats/test/IOMockitoMatchers.scala
@@ -12,10 +12,10 @@ trait IOMockitoMatchers extends MockitoMatchers {
   extension (stub: OngoingStubbing[IO[Unit]]) def thenReturnUnit: OngoingStubbing[IO[Unit]] = stub.thenReturn(IO.unit)
 
   extension [A](stub: OngoingStubbing[fs2.Stream[IO, A]])
-    def thenFailStream(error: Throwable) = stub.thenReturn(fs2.Stream.raiseError[IO](error))
-    def thenStream(items: A*)            = stub.thenReturn(fs2.Stream.emits(items.toList))
-    def thenStream(items: List[A])       = stub.thenReturn(fs2.Stream.emits(items))
-    def thenReturnEmptyStream            = stub.thenReturn(fs2.Stream.empty)
+    def thenFailStream(error: Throwable): OngoingStubbing[fs2.Stream[IO, A]] = stub.thenReturn(fs2.Stream.raiseError[IO](error))
+    def thenStream(items: A*): OngoingStubbing[fs2.Stream[IO, A]]            = stub.thenReturn(fs2.Stream.emits(items.toList))
+    def thenStream(items: List[A]): OngoingStubbing[fs2.Stream[IO, A]]       = stub.thenReturn(fs2.Stream.emits(items))
+    def thenReturnEmptyStream: OngoingStubbing[fs2.Stream[IO, A]]            = stub.thenReturn(fs2.Stream.empty)
 
   extension [A](stub: OngoingStubbing[IO[Option[A]]])
     def thenReturnNone: OngoingStubbing[IO[Option[A]]]           = stub.thenReturn(IO.none[A])
