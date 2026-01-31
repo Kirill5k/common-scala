d003c61f98b287aa86df6c227c83500a1c7c062d
kirill5k <immotional@aol.com>
2026-01-31 10:44:55 +0000

    Bump sbt version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val http4s   = "0.23.33"
     val circe    = "0.14.15"
     val sttp     = "3.11.0"
-    val sttp4    = "4.0.14"
+    val sttp4    = "4.0.15"
 
     val scalaTest = "3.2.19"
     val mockito   = "3.2.18.0"
